0099923f0be37700989633b538a95d6a98e7ae99
FirewallCrack <nl.260808@gmail.com>
2024-05-06 14:56:39 +0000

    wedw

---
+++ variabili.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+//VARIABILI//
+
+int main ()
+{
+
+    int a; // NUMERI INTERI
+    float b; // NUMERI CON VIRGOLA (MAX 6)
+    double c; // NUMERI CON VIRGOLA (MAX 15)
+
+    a = 2;
+    b = 3.14;
+    c = 2.9104;
+
+    printf("a: %d\n", a);   // %d x int
+    printf("b: %f\n", b);   // %f x float
+    printf("c: %lf\n", c);  // %lf x double
+
+    // nelle virgolette %x fuori il nome della variabile
+
+    a = 61;
+
+    printf("a: %d\na/b: %f\n", a, a/b);
+}
