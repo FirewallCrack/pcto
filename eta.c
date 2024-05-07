132190547526389fcfc38cb3c82338b2f528ded2
FirewallCrack <nl.260808@gmail.com>
2024-05-07 10:43:22 +0000

    pcto

--- eta.c
+++ eta.c
@@ -4,30 +4,20 @@ int main()
     int eta;
     printf("Quanti anni hai?\n");
     scanf("%d", &eta);
-    if (eta<18)
+    if (eta>18&&eta<120)
     {
-        if (eta<1)
-        {
-            printf("ERRORE! ETÀ NEGATIVA O IMPOSSIBILE: FAI IL SERIO\n");
-        }
-        else
-        {
-            printf("Hai %d anni, sei minorenne\n", eta);
-        }
+        printf("Hai %d anni, sei maggiorenne\n", eta);
     }
-    else
+    else if (eta> 120)
     {
-        if (eta>18)
-        {
-            if (eta>100)
-            {
-                printf("Ha ha ha che divertente. Non hai realmente %d anni\n", eta);
-            }
-            else 
-            {
-                printf("Hai %d anni, sei maggiorenne\n", eta);
-            }
-        }
-        
+        printf("Non credo tu abbia veramente %d anni, non sei divertente\n", eta);
+    }
+    else if (eta>0&&eta<18)
+    {
+        printf("Hai %d anni, non sei maggiorenne\n", eta);
+    }
+    else if (eta<0)
+    {
+        printf("Non è possibile che tu abbia %d anni, non fai ridere\n", eta);
     }
 }
